0dbce5998530584eb946e6f5d1f3aff74d45fab8
Ashes <asheskumar55@gmail.com>
2018-03-28 19:35:53 +0545

    aaa

--- index.php
+++ index.php
@@ -9,10 +9,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Insert Update Delete using Store Procedure in PHP MYSQL</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/style.css">
+	<?php include_once 'Tools/bootstrap.php' ?>
 </head>
 <body>
 	<div class="container box">
@@ -23,7 +20,7 @@
 		{
 			include_once 'Tools/Edit.php';
 		}
-		else
+		else 
 		{
 		?>
 	<!-- This is all the form -->
